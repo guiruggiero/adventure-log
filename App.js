e6f2d9b3e45268567efd667fd1db1e59f765e475
Gui Ruggiero <guilherme.ruggiero@gmail.com>
2020-12-21 03:24:53 -0500

    Kickoff

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
-// SI 669 - Final Project
-// Developed by Gui Ruggiero
-// Repo: https://github.com/SI669-classroom-f20/final-project-guiruggiero
-
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import { LoginScreen } from './LoginScreen';
+// import { HomeScreen } from './HomeScreen';
+// import { SettingsScreen } from './SettingsScreen';
 import { TimelineScreen } from './TimelineScreen';
 import { DiveScreen } from './DiveScreen';
+// import { JumpScreen } from './JumpScreen';
 // import { CameraScreen } from './CameraScreen'; // FLAG
 
 const Stack = createStackNavigator();
@@ -23,11 +22,11 @@ function App() {
         }}
       >
         <Stack.Screen name="Login" component={LoginScreen} />
-
+        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+        {/* <Stack.Screen name="Settings" component={SettingsScreen} /> */}
         <Stack.Screen name="Timeline" component={TimelineScreen} />
-
         <Stack.Screen name="Dive" component={DiveScreen} />
-
+        {/* <Stack.Screen name="Jump" component={DiveScreen} /> */}
         {/* <Stack.Screen name="Camera" component={CameraScreen} /> */}
       </Stack.Navigator>
     </NavigationContainer>
